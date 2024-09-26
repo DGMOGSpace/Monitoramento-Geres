67d5eca0edbef3f61b6bbd8ca587f487906a4c7b
Carlos Silva <88803987+carloosf@users.noreply.github.com>
2024-09-26 16:53:32 -0300

    update: add new style

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -26,26 +26,20 @@ const App = () => {
           className="h-full w-full flex flex-col bg-blue-400 bg-cover bg-center"
           style={{ backgroundImage: "url('bg_blue_home.png')" }}
         >
-          <div className="flex flex-col items-center justify-center flex-grow mt-10">
-            <img
-              className="w-60 absolute mt-14" // Use a classe do Tailwind para sombra
-              src="gpr_logo.png"
-              alt="Logo da GPR, destaque do aplicativo"
-              style={{ filter: "drop-shadow(0px 0px 5px rgba(255, 255, 255, 0.8))" }} // Aumentei a sombra
-            />
-          </div>
+          
 
-          <div className="grid md:grid-cols-2 gap-6 flex-grow">
-            <div className="flex justify-center items-center">
-              <AuthForm setLoading={setLoading} />
-            </div>
-            <div className="flex justify-center items-center">
+          <div className="grid md:grid-cols-2 gap-6 flex-grow bg-gradient-to-r from-white">
+          <div className="flex justify-center items-center">
               <img
                 className="md:block w-96"
-                src="data_vector.png"
+                src="gpr_logo.png"
                 alt="Ilustração relacionada a dados"
               />
             </div>
+            <div className="flex justify-center items-center">
+              <AuthForm setLoading={setLoading} />
+            </div>
+            
           </div>
 
           <footer className="flex justify-center items-center gap-5 bg-slate-50 p-4">
